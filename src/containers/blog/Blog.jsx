058e03c5a3ec7ffd587c74c794dde60cd9192088
Sumit Mishra <sumit894sumit@gmail.com>
2023-01-23 18:55:24 +0530

    added footer

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
-import './blog.css';
-import {card} from '../../Text'
+import React from "react";
+import "./blog.css";
+import { card } from "../../Text";
 
 const Blog = () => {
-  
   return (
     <div className="card-container">
-      
-      {card.map((value,index)=>{
-        return <div className={`card card-${index}`}>{value.a} </div>
-
+      {card.map((value, index) => {
+        return   <div className={`card card-${index}`}>{value.a} </div>;
       })}
-     
-    
     </div>
-  )
-}
+  );
+};
 
 export default Blog;
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,7 +3,15 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="footer-text">
+        <h1>Do you want to step in to the future before others</h1>
+         <button>Request Early Access</button>
+      </div>
+      <div className="footer-links">
+        
+      </div>
+    </div>
   )
 }
 
